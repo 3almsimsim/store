587872b3e54355a587c5da252b9623d73705b363
3almsimsim <139587020+3almsimsim@users.noreply.github.com>
2024-05-30 15:59:11 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const bar = document.getElementById('mob');
-const menu = document.getElementById('navlist');
-
-if (bar) {
-    bar.addEventListener('click', () => {
-        menu.classList.add('active');
-    })
-}
