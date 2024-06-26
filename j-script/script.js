d2b4a3ff620ab1f50abd70087376fe0fc6b92e15
3almsimsim <139587020+3almsimsim@users.noreply.github.com>
2024-06-26 22:57:58 +0300

    Add files via upload

--- j-script/script.js
+++ j-script/script.js
@@ -13,3 +13,25 @@ if (close) {
         menu.classList.remove('active');
     })
 }
+
+// cart
+
+const cart_icon = document.getElementById('cart');
+const cart_menu = document.querySelector('.cart_main');
+const cart_close = document.getElementsByClassName('close_cart');
+
+if (cart_icon) {
+    cart_icon.addEventListener('click', () => {
+        cart_menu.classList.add('active');
+        
+    })
+}
+
+if (cart_close) {
+    close_cart.addEventListener('click', () => {
+        cart_menu.classList.remove('active');
+    })
+}
+
+
+
