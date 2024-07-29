3a86b377c2abb983dd2e01adcbebd3b1fe98296b
3almsimsim <139587020+3almsimsim@users.noreply.github.com>
2024-07-29 19:06:56 +0300

    Update secondary_two.js

--- books/secondary_two.js
+++ books/secondary_two.js
@@ -12,6 +12,7 @@ const productsDOM = document.querySelector(".week-section-container");
 const twoPrepDOM = document.querySelector('.twoPrep-container');
 const two_secDOM = document.querySelector('.secondary_two_container');
 const fiveDOM = document.querySelector('.five-container');
+const image = document.getElementById("image");
 // const btns = document.querySelectorAll('.addTo-cart');
 // test
 const sixDOM = document.querySelector('.six-container');
