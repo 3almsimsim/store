6ebbac3598617d33d366304c11358139f1e025db
3almsimsim <139587020+3almsimsim@users.noreply.github.com>
2024-07-29 19:03:21 +0300

    Update schoolNotebook.js

--- back-to-school/notebook/schoolNotebook/schoolNotebook.js
+++ back-to-school/notebook/schoolNotebook/schoolNotebook.js
@@ -9,7 +9,7 @@ const cartItems = document.querySelector(".quantity");
 const cartTotal = document.querySelector(".total_price");
 const cartContent = document.querySelector(".cartContainer");
 const schoolNotebookDOM = document.querySelector(".product-box-container");
-const image = document.getElementById("item-img");
+const image = document.getElementById("image");
 // const btns = document.querySelectorAll('.addTo-cart');
 // test
 // const featuredProductsDOM = document.querySelector('.featured-container');
