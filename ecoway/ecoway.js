54c91f6e4d2001186f4b5da46cbddd50d297f84f
3almsimsim <139587020+3almsimsim@users.noreply.github.com>
2024-07-29 19:13:47 +0300

    Update ecoway.js

--- ecoway/ecoway.js
+++ ecoway/ecoway.js
@@ -287,6 +287,7 @@ class UI {
     Name_Items.value = [];
     amount.value = [];
     Price.value = [];
+    image.value = "";
   }
   removeItem(id) {
     cart = cart.filter((item) => item.id !== id);
